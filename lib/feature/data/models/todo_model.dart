87d27391deaa04c6f9c8aa5da1a1258ee4e98cae
Vinicius Queiroz <ti2@shomer.com.br>
2025-02-18 13:14:44 -0300

    feat: todo model from json added

--- lib/feature/data/models/todo_model.dart
+++ lib/feature/data/models/todo_model.dart
@@ -4,4 +4,14 @@ import 'package:todo_app_bloc/feature/domain/entities/todo_entity.dart';
 class TodoModel extends TodoEntity{
   const TodoModel({required super.id, required super.content, required super.title});
 
+
+  factory TodoModel.fromJson(Map<String,dynamic> json){
+    final todo = TodoEntity.fromJson(json);
+    return TodoModel(
+      id: todo.id,
+      title: todo.title,
+      content: todo.content,
+    );
+  }
+
 }
